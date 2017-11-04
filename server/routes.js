c4eeff84b1fa43e813c9e4a36b1a58498c78ed81
Edjo Brandão <edjo@haus-app.com>
2017-11-04 14:45:25 -0400

    fix middleware to es6 codestyle

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,14 @@
 module.exports = app => {
 
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    app.use((req, res, next) => {
+        res.header(`Access-Control-Allow-Origin`, `*`);
+        res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
         next();
     });
 
-	app.get(`/`, (req, res) => {
-		res.sendfile('./public/index.html');
-	});
+    app.get(`/`, (req, res) => {
+        res.sendfile('./public/index.html');
+    });
 
     app.get(`/questions`, (req, res) => {
 
